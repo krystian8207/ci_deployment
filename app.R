85329bd8523843e473b3d51a2853ff0c7066b6ac
Krystian Igras <krystian8207@gmail.com>
2023-04-23 22:44:34 +0200

    Initialize app repo.

---
+++ app.R
@@ -0,0 +1,50 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+source("utils.R")
+
+ui <- shinydashboardPlus::dashboardPage(
+  options = list(sidebarExpandOnHover = TRUE),
+  header = shinydashboardPlus::dashboardHeader(
+    title = "Sales App" |> shiny::h3(class = "dropdown", style = "display: inline;")
+  ),
+  sidebar = shinydashboardPlus::dashboardSidebar(
+    minified = TRUE, collapsed = FALSE,
+    shiny::selectInput("store", "Store", choices = 1:3, selected = 1),
+    shiny::selectInput("department", "Department", choices = 1:3, selected = 1),
+    shiny::selectInput(
+      "variables", "Variables",
+      choices = c("Date", "Temperature", "Fuel_Price", "CPI", "Unemployment", "IsHoliday", "Weekly_Sales", "Name"),
+      selected = c("Date", "Weekly_Sales"), multiple = TRUE
+    )
+  ),
+  body = shinydashboard::dashboardBody(
+    shiny::textOutput("title"),
+    shiny::tableOutput("sales_data")
+  ),
+  title = "DashboardPage"
+)
+
+server <- function(input, output, session) {
+
+  res_data <- reactiveVal(NULL)
+
+  output$title <- renderText({
+    data_header(input$store, input$department)
+  })
+
+  observeEvent(input$store, {
+    res_data(read_data(input$store))
+  }, ignoreNULL = TRUE)
+
+  output$sales_data <- renderTable({
+    req(input$department)
+
+    res_data() %>%
+      filter(Dept == input$department) %>%
+      subset_data(input$variables)
+  })
+}
+
+shinyApp(ui, server)
---
+++ utils.R
@@ -0,0 +1,15 @@
+data_header <- function(store, department) {
+  paste0("Showing data from store ", store, " and department ", department)
+}
+
+subset_data <- function(data, cols) {
+  valid_cols <- intersect(cols, colnames(data))
+  if (!length(valid_cols)) {
+    return(data)
+  }
+  data[, valid_cols, drop = FALSE]
+}
+
+read_data <- function(store) {
+  read.csv(paste0("walmart_", store, ".csv"))
+}
